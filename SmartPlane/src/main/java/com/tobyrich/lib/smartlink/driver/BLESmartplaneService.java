e17974770f6fa8e915e7a88d706015e9ff204075
Prashant Vaibhav <prashant.vaibhav@tobyrich.com>
2014-03-04 15:19:35 +0100

    Added ability to check battery charging status to the smartplane service
    Summary:
    The smartplane/chargestatus field is set to 0 when battery is not charging, and changes to 1 when charging.
    This patch adds a method updateChargingStatus() that can be called to check current battery charging status.
    Also added is a delegate to the BLESmartplaneService class to subsequently receive charging status.
    
    Test Plan: Cross fingers and hope it works. Untested.
    
    Reviewers: samit.vaidya
    
    Reviewed By: samit.vaidya
    
    Differential Revision: https://office.tobyrich.com/D6

--- SmartPlane/src/main/java/com/tobyrich/lib/smartlink/driver/BLESmartplaneService.java
+++ SmartPlane/src/main/java/com/tobyrich/lib/smartlink/driver/BLESmartplaneService.java
@@ -2,12 +2,21 @@ package com.tobyrich.lib.smartlink.driver;
 
 import com.tobyrich.lib.smartlink.BLEService;
 
+import java.lang.ref.WeakReference;
+
 /**
  * Created by pvaibhav on 13/02/2014.
  */
 public class BLESmartplaneService
     extends BLEService
 {
+    public interface Delegate {
+        void didStartChargingBattery();
+        void didStopChargingBattery();
+    }
+
+    public WeakReference<Delegate> delegate;
+
     public void setMotor(short speed) {
         if (speed > 254)
             speed = 254;
@@ -24,6 +33,10 @@ public class BLESmartplaneService
         writeInt8Value((byte) value, "smartplane/rudder");
     }
 
+    public void updateChargingStatus() {
+        updateField("smartplane/chargestatus");
+    }
+
     @Override
     public void detach() {
 
@@ -31,6 +44,15 @@ public class BLESmartplaneService
 
     @Override
     protected void didUpdateValueForCharacteristic(String c) {
+        if (delegate.get() == null)
+            return;
 
+        if (c.equalsIgnoreCase("smartplane/chargestatus")) {
+            int status = getUint8ValueForCharacteristic("smartplane/chargestatus");
+            if (status == 0)
+                delegate.get().didStopChargingBattery();
+            else
+                delegate.get().didStartChargingBattery();
+        }
     }
 }
